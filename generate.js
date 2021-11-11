14073df737fcca663ee65a04c57886407710af1a
Jenya.Seletsky <js646@me.com>
2021-11-11 13:53:48 -0700

    updated generate js

--- generate.js
+++ generate.js
@@ -10,6 +10,13 @@ let passwordValue = '';
 //Creating Password Generation
 const createPassword = () => {
     passwordValue = ''; 
+    let desiredLength =  prompt("Choose password length between 8 - 128");
+    if (isNaN(desiredLength)) {
+        desiredLength =  prompt("Must be a number above 8 and below 128");
+    }
+    console.log(desiredLength)
+   
+
 
     for (let i = 0; i < passwordLength; i++) {
         let number = Math.floor(Math.random() * characters.length);
