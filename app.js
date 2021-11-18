c8d0c0c33fa30e5d816c0cc07c27aa20c11944f6
Jenya.Seletsky <js646@me.com>
2021-11-17 18:13:09 -0700

    original quiz commit

---
+++ app.js
@@ -0,0 +1,136 @@
+//quiz class
+class Quiz{
+    constructor(questions){
+        this.score = 0;
+        this.questions = questions;
+        this.questionIndex = 0;
+    }
+
+    getQuestionIndex() { 
+        return this.questions[this.questionIndex];
+    }
+
+    guess(answer){
+        if(this.getQuestionIndex().isCorrectAnswer(answer)){
+            this.score++;
+        }
+        this.questionIndex++;
+    }
+
+    isEnded(){
+        return this.questionIndex === this.questions.length;
+    }
+}
+
+//question class methods
+class Question{
+    constructor(text, choices, answer){
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    isCorrectAnswer(choice) {
+        return this.answer === choice;
+    }
+} 
+
+// question displays
+function displayQuestion(){
+    if(quiz.isEnded){
+        showScores();
+    }else{
+
+        //questions
+        let questionElement = document.getElementById("questions");
+        console.log(quiz.getQuestionIndex)
+        questionElement.innerHTML = quiz.getQuestionIndex().text;
+
+        //options
+        let choices = quiz.getQuestionIndex().choices;
+        for(let i = 0; i <choices.length; i++) {
+            let choiceElement = document.getElementById ("choice" + i);
+            choiceElement.innerHTML = choices[i];
+            guess("btn" + i, choices[i], choiceElement);
+        }
+
+        showProgress();
+
+    }
+}
+
+//guess 
+function guess(id, guess){
+    let button = document.getElementById(id);
+    button.onclick = function(){
+        quiz.guess(guess);
+        displayQuestion();
+    }
+}
+
+//progress
+function showProgress() {
+    let currentQuestionNumber =quiz.questionIndex + 1;
+    let progressElement = document.getElementById("progress");
+    progressElement.innerHTML =
+    `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
+}
+
+//showScores
+function showScores() {
+    let quizEndHTML = 
+    `<h1> Quiz Completed</h1>
+     <h2 id="score"> You Scored: ${quiz.score} of ${quiz.questions.length}
+    <h2>
+    <div class="quiz-repeat">
+        <a href="index.html"> Take Quiz Again</a>
+    </div>
+    `;
+    let quizElement = document.getElementById("quiz");
+    quizElement.innerHTML = quizEndHTML;
+}
+
+// quiz Questions
+let questions = [
+    new Question(
+        "what color is the sky?", ["blue", "green", "red", "brown"], "blue"
+    ),
+    new Question(
+        "what color is the sky?", ["blue", "green", "red", "brown"], "blue"
+    ),
+    new Question(
+        "what color is the sky?", ["blue", "green", "red", "brown"], "blue"
+    ),
+    new Question(
+        "what color is the sky?", ["blue", "green", "red", "brown"], "blue"
+    )
+    
+];
+
+let quiz = new Quiz(questions);
+
+//display questions
+displayQuestion();
+
+// timer
+let time = 5;
+let quizTimeInMinutes = time * 60 * 60;
+quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown() {
+    let quizTimer = setInterval (function() {
+        if (quizTime <= 0) {
+            clearInterval(quizTimer);
+            showScores();
+        }else {
+            quizTime--;
+            let sec = Math.floor(quizTime % 60);
+            let min = Math.floor(quizTime / 60) % 60;
+            counting.textContent = `Time: ${min} : ${sec}`;
+        }
+    },1000)
+}
+
+startCountdown();
---
+++ newapp.js
@@ -0,0 +1,31 @@
+//reference elements
+
+var timerEl = document.getElementById("count-down");
+var progressEl = document.getElementById("progress");
+var questionBtnEl = document.getElementById("buttons");
+var currentQestionEl = document.getElementById("question");
+
+//Quiz Variables
+var score 
+var currentQuestion
+
+
+
+//logic
+function startQuiz(){
+    console.log("start quiz")
+
+
+}
+
+
+function endQuiz(){
+
+
+
+}
+
+//Initiate
+startQuiz();
+
+
