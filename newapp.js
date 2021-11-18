7a4970b3c52d9e61f0b31d3076e740d229be075c
Jenya.Seletsky <js646@me.com>
2021-11-17 18:43:04 -0700

    added hide start button

--- newapp.js
+++ newapp.js
@@ -4,6 +4,7 @@ var timerEl = document.getElementById("count-down");
 var progressEl = document.getElementById("progress");
 var questionBtnEl = document.getElementById("buttons");
 var currentQestionEl = document.getElementById("question");
+var startBtnEl = document.getElementById("startBtn");
 
 //Quiz Variables
 var score 
@@ -16,6 +17,33 @@ function startQuiz(){
     console.log("start quiz")
 
 
+    // hide start buttons
+    startBtnEl.style.display = "none";
+
+    //start timerEl
+
+    let time = 5;
+let quizTimeInMinutes = time * 60 * 60;
+quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown() {
+    let quizTimer = setInterval (function() {
+        if (quizTime <= 0) {
+            clearInterval(quizTimer);
+            showScores();
+        }else {
+            quizTime--;
+            let sec = Math.floor(quizTime % 60);
+            let min = Math.floor(quizTime / 60) % 60;
+            counting.textContent = `Time: ${min} : ${sec}`;
+        }
+    },1000)
+
+    //show question
+
+
 }
 
 
@@ -26,6 +54,7 @@ function endQuiz(){
 }
 
 //Initiate
-startQuiz();
+startBtnEl.addEventListener("click", startQuiz)
+
 
 
