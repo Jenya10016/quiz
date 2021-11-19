f3d9589c2433d2d9f60f3720600cd4a912a115bd
Jenya.Seletsky <js646@me.com>
2021-11-18 21:30:40 -0700

    changes

--- newapp.js
+++ newapp.js
@@ -3,21 +3,34 @@
 var timerEl = document.getElementById("count-down");
 var progressEl = document.getElementById("progress");
 var questionBtnEl = document.getElementById("buttons");
-var currentQestionEl = document.getElementById("question");
+var currentQuestionEl = document.getElementById("question");
 var startBtnEl = document.getElementById("startBtn");
 
 //Quiz Variables
 var quizTime = 120
 var quizTimer
-var currentQuestion
-var timerID
 
-
+var currentQuestionIndex = 0;
+var questions = [{
+    text: "what color is the sky?",
+    choices: ["blue", "green", "red", "brown", 1],
+    answer: "blue"
+},
+{
+    text: "what color is fire?",
+    choices: ["blue", "green", "red", "brown"],
+    answer: "red"
+},
+{
+    text: "what color is grass?",
+    choices: ["blue", "green", "red", "brown"],
+    answer: "green" 
+}
+]
 
 //logic
-function startQuiz(){
+function startQuiz() {
     console.log("start quiz")
-
 
     // hide start buttons
     startBtnEl.style.display = "none";
@@ -25,29 +38,76 @@ function startQuiz(){
 
     //start timerEl
 
-    quizTimer = setInterval (function() {
+    quizTimer = setInterval(function () {
         if (quizTime <= 0) {
             clearInterval(quizTimer);
             showScores();
 
 
-        }else {
+        } else {
             quizTime--;
             let sec = Math.floor(quizTime % 60);
             let min = Math.floor(quizTime / 60) % 60;
             timerEl.textContent = `Time: ${quizTime}`;
         }
-    },1000)
+    }, 1000)
 
-    
 
     //show question
+    showQuestion()
+}
 
+function showQuestion() {
+    //grab question
+    var ques = questions[currentQuestionIndex]
+
+    //update html
+    currentQuestionEl.textContent = ques.text
+    questionBtnEl.innerHTML = ""
+
+    ques.choices.forEach(element => {
+
+        //create button
+        var questionBtn = document.createElement("button")
+
+        //add text to button
+        questionBtn.textContent = element
+
+        //append button questionBtnEl
+        questionBtnEl.appendChild(questionBtn)
+
+
+        //add class atribute to button
+    questionBtn.setAttribute("class", "btn")
+
+
+    questionBtnEl.onclick = handleQuestionClick
+
+
+        // add eventlistener
+
+
+
+
+    })
+function handleQuestionClick(){
+    //check if right or wrong answer
+
+    //subtract time if wrong
+
+    // check to show if there are more questions
+
+
+    currentQuestionIndex ++;
+    showQuestion()
+}
 
 }
 
 
-function endQuiz(){
+
+
+function endQuiz() {
 
 
 
